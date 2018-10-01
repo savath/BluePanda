7b8ec8086dee692918b05feea54d815457783bb1
savath <savathsaepoo@gmail.com>
2018-10-01 12:52:09 -1000

    Create modal.js

---
+++ Example/modal.js
@@ -0,0 +1,7 @@
+function clickMe() {
+  document.getElementById('modal01').style.display='block';
+}
+      
+function imageGrow() {
+  this.document.getElementById('modal01').style.display='none'
+}
